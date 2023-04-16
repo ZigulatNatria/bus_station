f689e902ccdd6d13e583ad51a0bf4692e020e9dd
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-04-16 23:39:19 +0500

    Исправлены косяки в галлерее, добавлено поле хранения файла

--- bus/station/models.py
+++ bus/station/models.py
@@ -190,6 +190,7 @@ class Insurer(models.Model):
 class Service(models.Model):
     name = models.CharField(verbose_name='Название услуги', max_length=200)
     text = HTMLField(verbose_name='Текст', null=True, blank=True)
+    file = models.FileField(verbose_name='Файл', upload_to='media', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Услуги'
